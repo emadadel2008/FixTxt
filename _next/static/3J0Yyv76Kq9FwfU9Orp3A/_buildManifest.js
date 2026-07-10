603b456925f3afcb161ab003f28db5665b0f5a70
GitHub Deploy <action@github.com>
2026-07-10 19:55:08 +0300

    Deploy modern theme to GitHub Pages

---
+++ _next/static/3J0Yyv76Kq9FwfU9Orp3A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/268-53a3e0926f400abf.js","static/chunks/pages/index-63780318accdc4db.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
